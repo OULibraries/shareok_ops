edc2669b0200618ddc89357c2c59e3de4ba78b3f
Logan Cox <lmc@ou.edu>
2015-11-11 16:17:39 -0600

    script to run the 3.x to 5.x database upgrade

---
+++ scripts/shareok-upgrade-db.sh
@@ -0,0 +1,23 @@
+#
+# Load a copy of the SHAREOK db and then upgrade it 
+#
+. /vagrant/etc/conf.sh
+
+# Remove existing DB and install fresh export
+$DSPACE_RUN/bin/dspace database clean
+psql -U $DB_NAME -h $DB_HOST $DB_NAME < $DB_EXPORT
+
+# Upgrade Database
+$DSPACE_RUN/bin/dspace database migrate
+
+# Remove deprecated Database Browse Tables
+$DSPACE_RUN/bin/dspace index-db-browse -f -d
+
+# Delete contents of legacy browse table
+cat <<EOF | psql -U $DB_NAME -h $DB_HOST $DBNAME
+DELETE FROM COMMUNITIES2ITEM
+EOF
+
+
+# update the discovery index with the new data
+$DSPACE_RUN/bin/dspace  index-discovery -b
