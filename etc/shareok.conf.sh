0be2ddec8bcce429fd09563a47d7a3ba763da1cd
Jason Sherman <jsn.sherman@gmail.com>
2015-11-13 11:38:29 -0600

    capturing initial work on grabbing db variables from other files.

--- etc/shareok.conf.sh
+++ etc/shareok.conf.sh
@@ -20,5 +20,13 @@ DB_NAME_SRC='shareok_prod_3x'
 DB_NAME=dspace
 DB_PASS=dspace
 
+#DS_CFG=${DSPACE_RUN}/config/dspace.cfg
+#DS_DB_URL=`cat $DS_CFG | grep ^db.url | cut -d "=" -f 2 | xargs`
+#DS_DB_HOST=`echo $DS_DB_URL | cut -d "/" -f 3 | cut -d ":" -f 1`
+#DS_DB_PORT=`echo $DS_DB_URL | cut -d "/" -f 3 | cut -d ":" -f 2`
+#DS_DB=`echo $DS_DB_URL | cut -d "/" -f 4 | xargs`
+#DS_USER=`cat $DS_CFG | grep ^db.username | cut -d "=" -f 2 | xargs`
+#DS_PASS=`cat $DS_CFG | grep ^db.password | cut -d "=" -f 2 | xargs`
+
 
 DB_EXPORT=/vagrant/downloads/shareokprod-dump.sql
