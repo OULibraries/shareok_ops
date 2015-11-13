fa0d30590f39c20d9f7e83253f82b426b4b93f93
Jason Sherman <jsn.sherman@gmail.com>
2015-11-13 14:11:49 -0600

    Captured work on nabbing variables for build.prop.

--- etc/shareok.conf.sh
+++ etc/shareok.conf.sh
@@ -3,6 +3,8 @@ MAVEN_PROFILE=vagrant
 MAVEN=mvn
 ANT=ant
 TOMCAT=/usr/share/tomcat
+DS_CFG=/vagrant/shareok_dspace/amzprod.properties
+DS_DB_URL=`cat $DS_CFG | grep ^db.url | cut -d "=" -f 2 | xargs`
 export ANT_OPTS="-Xms128m -Xmx800m"
 
 # DSpace Paths
@@ -12,21 +14,19 @@ DSPACE_BACKUP=/var/local/backups/dspace
 
 # DSpace Database Configuration
 DB_HOST=localhost
+#DB_HOST=`echo $DS_DB_URL | cut -d "/" -f 3 | cut -d ":" -f 1`
+
 DB_ADMIN=libacct
 DB_ADMIN_PASS=libacct
 
 # Currently using the same name for user and db 
 DB_NAME_SRC='shareok_prod_3x'
 DB_NAME=dspace
+#DB_NAME=`echo $DS_DB_URL | cut -d "/" -f 4 | xargs`
 DB_PASS=dspace
+#DB_PASS=`cat $DS_CFG | grep ^db.password | cut -d "=" -f 2 | xargs`
 
-#DS_CFG=${DSPACE_RUN}/config/dspace.cfg
-#DS_DB_URL=`cat $DS_CFG | grep ^db.url | cut -d "=" -f 2 | xargs`
-#DS_DB_HOST=`echo $DS_DB_URL | cut -d "/" -f 3 | cut -d ":" -f 1`
-#DS_DB_PORT=`echo $DS_DB_URL | cut -d "/" -f 3 | cut -d ":" -f 2`
-#DS_DB=`echo $DS_DB_URL | cut -d "/" -f 4 | xargs`
-#DS_USER=`cat $DS_CFG | grep ^db.username | cut -d "=" -f 2 | xargs`
-#DS_PASS=`cat $DS_CFG | grep ^db.password | cut -d "=" -f 2 | xargs`
+#DB_USER=`cat $DS_CFG | grep ^db.username | cut -d "=" -f 2 | xargs`
 
 
 DB_EXPORT=/vagrant/downloads/shareokprod-dump.sql
